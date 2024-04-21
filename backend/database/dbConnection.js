49e79c249299d94189d33985ddb99b05b3a9021e
Nishanshu Tiwari <nishanshutiwari8914@gmail.com>
2024-04-21 17:27:10 +0530

    commit

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 export const dbConnection=()=>{
 
     mongoose.connect(process.env.MONGO_URI,{
-        dbName: "MERN_JOB_SEEKING_WEBAPP"
+        // dbName: "MERN_JOB_SEEKING_WEBAPP",
+
 
     }).then(()=>{
         console.log("Connected to db sucessfully")
